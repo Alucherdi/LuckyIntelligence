50d4c039123bef179b98407e74112e3ce6538780
TOSHIBA\Ivan <irosales@luckyintelligence.com>
2018-08-15 10:27:55 -0500

    Added scrollbar to menu, fixed logos of allies and clients

--- Sitio Web Lucky/js/menu.js
+++ Sitio Web Lucky/js/menu.js
@@ -16,7 +16,26 @@ function main(){
 				left: '-100%'
 			});
 		}
- 
+
+		$('.button').click(function(){
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		});
 	});
- 
 };
+
+ $(document).on("click",function(e) {
+
+ 	var menu = $(".menu_bar"),
+ 		container = $("nav");
+
+ 	if ((!container.is(e.target) && container.has(e.target).length === 0) && 
+ 		(!menu.is(e.target) && menu.has(e.target).length === 0)  ){ 
+ 		contador = 1;
+ 		$('nav').animate({
+			left: '-100%'
+		});               
+ 	}
+ });
